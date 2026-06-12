50f98f66014b76c5eb9c07e9d984a0f4d6656c2c
Dha-nisa <dhanisarajesh@gmail.com>
2026-06-12 14:27:24 +0530

    Resolved merge conflicts and updated project

--- src/Phase3/completed_consumer.py
+++ src/Phase3/completed_consumer.py
@@ -1,28 +1,27 @@
-# completed_consumer.py
-
 from kafka import KafkaConsumer, KafkaProducer
 import json
 
+# Consumer: Reads from ride.events
 consumer = KafkaConsumer(
     'ride.events',
     bootstrap_servers='localhost:9092',
-    auto_offset_reset='earliest',
+    auto_offset_reset='latest',  # Read only new messages
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
 
+# Producer: Writes only completed rides to ride.completed
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda x: json.dumps(x).encode('utf-8')
 )
 
-print("Completed Ride Consumer Started")
+print("Completed Ride Consumer Started...")
 
 for message in consumer:
 
     ride = message.value
 
-    print("Received:", ride)
-
+    # Filter only COMPLETED rides
     if ride["status"] == "COMPLETED":
 
         producer.send(
@@ -30,4 +29,6 @@ for message in consumer:
             value=ride
         )
 
-        print("Forwarded Completed Ride:", ride["ride_id"])
+        producer.flush()
+
+        print(f"Forwarded Completed Ride: {ride['ride_id']}")
